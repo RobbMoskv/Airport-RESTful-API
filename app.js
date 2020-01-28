7388a33ddc0f063aee9d8e1d514e20a6b648a6a1
Robb Moskv <fabrizio_39@hotmail.com>
2020-01-28 15:50:30 +0100

    first api get route

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ require('dotenv').config();
 
 const express = require('express');
 
+const airportRouter = express.Router();
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+airportRouter.route('/airports')
+  .get((req, res) => {
+    const response = { hello: 'Hi from API' };
+    res.json(response);
+  });
+
+app.use('/api', airportRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome Gentleman');
