93c620c72ed583ea3bff29393bed94c45a64bdd4
Robb Moskv <fabrizio_39@hotmail.com>
2020-01-28 18:18:29 +0100

    add inital db config

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 const airportRouter = express.Router();
 const app = express();
 const port = process.env.PORT || 3000;
+const Airport = require('./models/AirportModel');
 
+// REMOVE: DB
+const dbuser = process.env.DBUSER;
+const dbpw = process.env.PW;
+const db = process.env.DB;
+const url = `mongodb://${dbuser}:${dbpw}@127.0.0.1:27017/${db}`;
+
+mongoose.connect(url, { useNewUrlParser: true }).catch((err) => {
+  console.log(`Database connections failed - ${err}`);
+});
 
 airportRouter.route('/airports')
   .get((req, res) => {
-    const response = { hello: 'Hi from API' };
-    res.json(response);
+    Airport.find((err, airports) => {
+      if (err) {
+        return res.send(err);
+      }
+
+      return res.json(airports);
+    });
   });
 
 app.use('/api', airportRouter);
 
 app.get('/', (req, res) => {
-  res.send('Welcome Gentleman');
+  res.send('Welcome to the Airport API');
 });
 
 app.listen(port, () => {
