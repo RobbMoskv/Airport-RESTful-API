38e7aa3ebf53917540cf81498c122307cf5d1d27
Robb Moskv <fabrizio_39@hotmail.com>
2020-01-30 13:45:32 +0100

    exclude route part

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Airport = require('./models/AirportModel');
+const airportRouter = require('./router/airportRoutes')(Airport);
 
-const airportRouter = express.Router();
 const app = express();
 const port = process.env.PORT || 3000;
-const Airport = require('./models/AirportModel');
 
 // REMOVE: DB
 const dbuser = process.env.DBUSER;
@@ -18,16 +19,8 @@ mongoose.connect(url, { useNewUrlParser: true }).catch((err) => {
   console.log(`Database connections failed - ${err}`);
 });
 
-airportRouter.route('/airports')
-  .get((req, res) => {
-    Airport.find((err, airports) => {
-      if (err) {
-        return res.send(err);
-      }
-
-      return res.json(airports);
-    });
-  });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/api', airportRouter);
 
