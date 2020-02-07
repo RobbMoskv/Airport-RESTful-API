88005e321085278fc7beffa515fa55d7c5100d2e
Robb Moskv <fabrizio_39@hotmail.com>
2020-02-07 13:51:33 +0100

    install swagger for node and create route

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
 const Airport = require('./models/AirportModel');
 const airportRouter = require('./router/airportRoutes')(Airport);
 
+const openapispec = YAML.load('./openapidoc.yaml');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -22,7 +26,12 @@ mongoose.connect(url, { useNewUrlParser: true }).catch((err) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// REST API's
 app.use('/api', airportRouter);
+
+// OpenAPI Documentation
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapispec));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Airport API');
