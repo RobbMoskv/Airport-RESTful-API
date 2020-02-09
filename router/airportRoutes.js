cdfee84626a519b84fb2d30018d9e7017d12831f
Robb Moskv <fabrizio_39@hotmail.com>
2020-02-09 20:38:50 +0100

    exclude logic into controller

--- router/airportRoutes.js
+++ router/airportRoutes.js
@@ -1,107 +1,23 @@
 /* eslint-disable no-param-reassign */
 const express = require('express');
+const airportController = require('../controllers/airportController');
 
 function routes(Airport) {
   const airportRouter = express.Router();
+  const controller = airportController(Airport);
 
+  // All airports
   airportRouter.route('/airports')
-    .get((req, res) => {
-      // Filtering
-      const query = {};
-      if (req.query.country) {
-        query.country = req.query.country;
-      }
-
-      Airport.find(query, (err, airports) => {
-        if (err) {
-          return res.send(err);
-        }
-
-        return res.json(airports);
-      });
-    })
-    .post((req, res) => {
-      const airport = new Airport(req.body);
-      airport.save();
-
-      return res.status(201).json(airport);
-    });
+    .get(controller.get)
+    .post(controller.post);
 
   // Single Airport
-  airportRouter.use('/airports/:id', (req, res, next) => {
-    Airport.findById(req.params.id, (err, airport) => {
-      if (err) {
-        return res.send(err);
-      }
-      if (airport) {
-        req.airport = airport;
-        return next();
-      }
-      return res.sendStatus(404);
-    });
-  });
+  airportRouter.use('/airports/:id', controller.middleware);
   airportRouter.route('/airports/:id')
-    .get((req, res) => res.json(req.airport))
-    .put((req, res) => {
-      const { airport } = req;
-
-      airport.code = req.body.code;
-      airport.lat = req.body.lat;
-      airport.lon = req.body.lon;
-      airport.name = req.body.name;
-      airport.city = req.body.city;
-      airport.state = req.body.state;
-      airport.country = req.body.country;
-      airport.woeid = req.body.woeid;
-      airport.tz = req.body.tz;
-      airport.phone = req.body.phone;
-      airport.type = req.body.type;
-      airport.email = req.body.email;
-      airport.url = req.body.url;
-      airport.runway_length = req.body.runway_length;
-      airport.elev = req.body.elev;
-      airport.icao = req.body.icao;
-      airport.direct_flights = req.body.direct_flights;
-      airport.carriers = req.body.carriers;
-
-      req.airport.save((err) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(airport);
-      });
-    })
-    .patch((req, res) => {
-      const { airport } = req;
-
-      // eslint-disable-next-line no-underscore-dangle
-      if (req.body._id) {
-        // eslint-disable-next-line no-underscore-dangle
-        delete req.body._id;
-      }
-
-      // Update all existing attributes in req.body
-      Object.entries(req.body).forEach((item) => {
-        const key = item[0];
-        const value = item[1];
-        airport[key] = value;
-      });
-
-      req.airport.save((err) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(airport);
-      });
-    })
-    .delete((req, res) => {
-      req.airport.remove((err) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.sendStatus(204);
-      });
-    });
+    .get(controller.getSingle)
+    .put(controller.put)
+    .patch(controller.patch)
+    .delete(controller.remove);
 
   return airportRouter;
 }
