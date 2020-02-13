8eb2185a8b16917b74f25a33ff4555bc7b86b76d
Robb Moskv <fabrizio_39@hotmail.com>
2020-02-13 20:21:43 +0100

    create unit test for airportcontroller > post

---
+++ tests/airportController.spec.js
@@ -0,0 +1,39 @@
+const should = require('should');
+const sinon = require('sinon');
+const airportController = require('../controllers/airportController');
+
+describe('Airport Controller', () => {
+  describe('GetAll', () => {
+
+  });
+
+  describe('GetSingle', () => {
+
+  });
+
+  describe('Post', () => {
+    it('should not allow empty Name on post', () => {
+      const Airport = function (airport) { this.save = () => {}};
+
+      const req = {
+        body: {
+          country: 'Germany',
+        }
+      };
+
+      const res = {
+        status: sinon.spy(),
+        send: sinon.spy(),
+        json: sinon.spy(),
+      };
+
+      const controller = airportController(Airport);
+      controller.post(req, res);
+
+      // Should throw a 'Bad Request'
+      res.status.calledWith(400).should.equal(true, `Incorrect Status: ${res.status.args[0][0]}`);
+      // Return error message
+      res.send.calledWith('Name is required').should.equal(true);
+    });
+  });
+});
